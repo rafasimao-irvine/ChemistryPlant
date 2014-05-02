b4d779eaaf0a264b4d851718d4ce1699dd383a0a
rafael simao <raficher@gmail.com>
2014-05-02 11:33:50 -0700

    first commit

---
+++ Assets/Scripts/Molecule.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Molecule : MonoBehaviour {
+
+	public string type;
+	protected float range = 1.0f;
+	protected float orientation = 0.12f;
+
+	protected float moved = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (moved > range) {
+			moved = 0;
+			orientation = -orientation;
+		
+		} else moved += Time.deltaTime;
+
+		transform.Translate(orientation * Vector3.up * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	public static int count;
+
+	void Start()
+	{
+		count = 0;
+		countText.text = "Count: "+count.ToString();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() 
+	{
+		countText.text = "Count: "+count.ToString();
+		if(count >= 15)
+			winText.text = "YOU WIN!!!!";
+	}
+
+
+}
